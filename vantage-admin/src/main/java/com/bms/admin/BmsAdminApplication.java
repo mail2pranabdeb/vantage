658d1c803731c374a22f19b793090701c387e52b
Pranab Debnath <mail2pranabdebdeb@gmail.com>
2026-03-13 19:38:02 +0200

    "fix-jpa-repositories-scan

--- vantage-admin/src/main/java/com/bms/admin/BmsAdminApplication.java
+++ vantage-admin/src/main/java/com/bms/admin/BmsAdminApplication.java
@@ -2,8 +2,10 @@ package com.pd.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.pd")
+@EnableJpaRepositories(basePackages = "com.pd.modules")
 public class BmsAdminApplication {
 
 	public static void main(String[] args) {
